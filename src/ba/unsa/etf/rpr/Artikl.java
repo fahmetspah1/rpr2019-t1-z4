a80845ad6d26f3f081722e43d88b3029eef70a51
Fahira Ahmetspahic <fahmetspah1@etf.unsa.ba>
2019-10-22 19:27:20 +0200

    Klase artikl i korpa zavrsene.

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,28 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    public String naziv = new String ();
+    public String kod = new String ();
+    public int cijena;
+    public Artikl () {}
+    public Artikl (String naziv, String kod, int cijena) {
+        Postavi(naziv,kod,cijena);
+    }
+    public void Postavi (String naziv, String kod, int cijena) {
+        this.naziv=naziv;
+        this.kod=kod;
+        this.cijena=cijena;
+    }
+
+    public String getNaziv () {
+        return this.naziv;
+    }
+
+    public String getKod () {
+        return this.kod;
+    }
+
+    public int getCijena () {
+        return this.cijena;
+    }
 }
--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,46 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    Artikl [] artikli = new Artikl [50];
+
+    public boolean dodajArtikl (Artikl novi) {
+        int i=0;
+        while (artikli[i]!=null) {
+            i=i+1;
+        }
+        artikli[i]=novi;
+        return true;
+    }
+
+    public Artikl izbaciArtiklSaKodom (String kod) {
+        Artikl novi = new Artikl();
+        int i=0;
+        while (artikli[i]!=null) {
+            if(kod.equals(artikli[i].getKod())) {
+                novi=artikli[i];
+                int j=i;
+                while(artikli[j]!=null) {
+                    artikli[j]=artikli[j+1];
+                    j=j+1;
+                }
+            }
+            i=i+1;
+        }
+        artikli[i-1]=null;
+        return novi;
+    }
+
+    public Artikl [] getArtikli () {
+        return artikli;
+    }
+
+    public int dajUkupunuCijenuArtikala () {
+        int iznos=0;
+        int i=0;
+        while (artikli[i]!=0) {
+            iznos = iznos + artikli[i].getCijena();
+            i = i + 1;
+        }
+        return iznos;
+    }
 }
