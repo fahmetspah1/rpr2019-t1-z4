40835bfda3303107db569d8dbdd408191e8abda3
Fahira Ahmetspahic <fahmetspah1@etf.unsa.ba>
2019-10-23 12:29:06 +0200

    Novi testovi

---
+++ test/ba/unsa/etf/rpr/KorpaTest.java
@@ -0,0 +1,24 @@
+package ba.unsa.etf.rpr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KorpaTest {
+
+    @Test
+    void dodajArtikl() {
+    }
+
+    @Test
+    void izbaciArtiklSaKodom() {
+    }
+
+    @Test
+    void getArtikli() {
+    }
+
+    @Test
+    void dajUkupnuCijenuArtikala() {
+    }
+}
